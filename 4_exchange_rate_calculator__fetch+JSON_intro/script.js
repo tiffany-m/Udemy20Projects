4ca00d6b083ff0b7caff307f0199456b0e01e90b
Tiffany <tiffmollica@gmail.com>
2021-08-05 17:26:18 -0400

    practice api fetch

---
+++ 4_exchange_rate_calculator__fetch+JSON_intro/script.js
@@ -0,0 +1,11 @@
+// PRACTICE WITH items.json FILE
+
+// function calculate() {
+//   fetch('items.json')
+//     .then(res => res.json())
+//     .then(data => document.body.innerHTML = data[0].text)
+// }
+// calculate()  
+
+// fetch makes a get request, when its done fetching it returns a promise, we catch a promise with .then, which gives a response, res.json formats the response into json, a second .then we get another promise back and we actually get the data
+
