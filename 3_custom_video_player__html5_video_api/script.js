5f38be6efc0e72958fbfd41ff390a7f2f1d27d1c
Tiffany <tiffmollica@gmail.com>
2021-08-11 17:36:08 -0400

    finished project

--- 3_custom_video_player__html5_video_api/script.js
+++ 3_custom_video_player__html5_video_api/script.js
@@ -24,12 +24,26 @@ function updatePlayIcon() {
 
 // Update Progress & Timestamp
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100
+
+  // Get Minuets
+  let mins = Math.floor(video.currentTime / 60)
+  if(mins < 10) {
+    mins = '0' + String(mins)
+  }
+
+  // Get Seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`
 }
 
 // Set Video Time to Progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100 // + sign makes it a number
 }
 
 // Stop Video
