5b99babbcc5c87e0445f9fcd6caa2259b2841b8f
Tiffany <67019104+tiffany-m@users.noreply.github.com>
2022-12-27 10:35:20 -0500

    fixed logic

--- 1_form_validator/script.js
+++ 1_form_validator/script.js
@@ -12,6 +12,19 @@ function showError(input, message) {
   small.innerText = message
 }
 
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success'
+}
+
+// Check email is valid
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(String(email).toLowerCase())
+}
+
 // Event Listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault() 
@@ -20,5 +33,25 @@ form.addEventListener('submit', function(e) {
     showError(username, 'Username is required.')
   } else {
     showSuccess(username)
+  }
+
+  if (email.value === '') {
+    showError(email, 'Email is required.');
+  } else if(!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid.')
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === '') {
+    showError(password, 'Password is required.');
+  } else {
+    showSuccess(password)
+  }
+
+  if (password2.value === '') {
+    showError(password2, 'Password 2 is required.');
+  } else {
+    showSuccess(password2)
   }
 })
