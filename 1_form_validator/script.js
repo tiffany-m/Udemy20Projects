8cf11821262a1671c89b857358ccaafe022b58ff
Tiffany <67019104+tiffany-m@users.noreply.github.com>
2022-12-27 10:35:20 -0500

    changed if statements to functions to make code cleaner

--- 1_form_validator/script.js
+++ 1_form_validator/script.js
@@ -25,33 +25,52 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase())
 }
 
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) { // .foreach loops through array, could also do this with a for loop instead
+    if(input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required.`) // id is the id in HTML for each input
+    } else {
+      showSuccess(input)
+    }
+  })
+} 
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+
 // Event Listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault() 
+  
+  checkRequired([username, email, password, password2])
 
-  if(username.value === '') {
-    showError(username, 'Username is required.')
-  } else {
-    showSuccess(username)
-  }
+  // First way of doing it, new way is cleaner and easier to maintain!
+  // if(username.value === '') {
+  //   showError(username, 'Username is required.')
+  // } else {
+  //   showSuccess(username)
+  // }
 
-  if (email.value === '') {
-    showError(email, 'Email is required.');
-  } else if(!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid.')
-  } else {
-    showSuccess(email);
-  }
+  // if (email.value === '') {
+  //   showError(email, 'Email is required.');
+  // } else if(!isValidEmail(email.value)) {
+  //   showError(email, 'Email is not valid.')
+  // } else {
+  //   showSuccess(email);
+  // }
 
-  if (password.value === '') {
-    showError(password, 'Password is required.');
-  } else {
-    showSuccess(password)
-  }
+  // if (password.value === '') {
+  //   showError(password, 'Password is required.');
+  // } else {
+  //   showSuccess(password)
+  // }
 
-  if (password2.value === '') {
-    showError(password2, 'Password 2 is required.');
-  } else {
-    showSuccess(password2)
-  }
+  // if (password2.value === '') {
+  //   showError(password2, 'Password 2 is required.');
+  // } else {
+  //   showSuccess(password2)
+  // }
 })
